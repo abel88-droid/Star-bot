9019599effd7f1168f7f550ed92e453cf399297f
Ash_in <83132070+abel88-droid@users.noreply.github.com>
2025-07-24 19:19:14 +0530

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,7 +3,7 @@ const { Events } = require('discord.js');
 module.exports = {
     name: Events.GuildMemberRemove,
     async execute(member) {
-        const channelId = '839905184154517597'; // Change to your leave channel ID
+        const channelId = '1390369076609814538'; // Change to your leave channel ID
 
         try {
             const channel = await member.guild.channels.fetch(channelId); // Fetch instead of cache lookup
